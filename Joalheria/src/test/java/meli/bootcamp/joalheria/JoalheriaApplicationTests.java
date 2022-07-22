88089c1db0146e6c33947ecf31187fee253d9854
felipeticiani-meli <felipe.ticiani@mercadolivre.com>
2022-07-22 11:27:06 -0300

    New project Joalheria

---
+++ Joalheria/src/test/java/meli/bootcamp/joalheria/JoalheriaApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.joalheria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoalheriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
