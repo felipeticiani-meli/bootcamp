355ecedfaa367e87b29fa185952ca34ae5fcb28d
felipeticiani-meli <felipe.ticiani@mercadolivre.com>
2022-07-25 18:11:33 -0300

    New project: QA Bugs

---
+++ QA Bugs/src/test/java/meli/bootcamp/qabugs/QabugsApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.qabugs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QabugsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
